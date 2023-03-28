29d19b21ce548e0704d76b475fa696ecfd2a26ce
Darice Sousa <daricesousa@gmail.com>
2023-03-27 23:51:35 -0300

    save finances with GetStorage

--- lib/app/modules/finance/finance_controller.dart
+++ lib/app/modules/finance/finance_controller.dart
@@ -1,11 +1,32 @@
 import 'package:finance_app/app/models/finance_model.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
 class FinanceController extends GetxController {
   final finances = <FinanceModel>[].obs;
+  final _storage = GetStorage();
+
+  @override
+  void onInit() {
+    _loadFinances();
+    ever<List<FinanceModel>>(finances, (f) {
+      _saveFinances();
+    });
+    super.onInit();
+  }
 
   void addFinance(FinanceModel finance) {
     finances.add(finance);
   }
-  
+
+  void _saveFinances() {
+    final data = finances.map((e) => e.toMap()).toList();
+    _storage.write("finances", data);
+  }
+
+  void _loadFinances() {
+    final data = _storage.read("finances") ?? [];
+    final elements = data.map<FinanceModel>((e) => FinanceModel.fromMap(e));
+    finances.assignAll(elements);
+  }
 }
